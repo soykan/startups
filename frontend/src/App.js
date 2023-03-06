8b63316f111e1a0a3394d62f03809f9e1ac57bf7
soykan <soykanerturk@gmail.com>
2023-03-07 01:17:04 +0300

    implementations about search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,23 +20,21 @@ function App() {
 
 const Search = () => {
   const [foundStartups, setFoundStartups] = React.useState([]);
-
-  const searchStartups = (event) => {
+  const handleSearchFormSubmit = (event) => {
     event.preventDefault();
     setFoundStartups(searchStartup(event.target.elements.startupName.value));
   } 
 
+
   return (
     <div>
-      <form onSubmit={(event) => searchStartups(event)}>
+      <form onSubmit={(event) => handleSearchFormSubmit(event)}>
         <input type="text" placeholder="startup name" name="startupName" />
         <input type="submit" value="Search" />
       </form>
       <div>
         {foundStartups.map((startup) => {
-          return (
-            <StartupDetails startup={startup} />
-          )
+          return <StartupDetailsInSearchResults startup={startup} />
         })
         }
       </div>
@@ -45,7 +43,7 @@ const Search = () => {
 }
 
 
-const StartupDetails = (props) => (
+const StartupDetailsInSearchResults = (props) => (
   <div>
     <h3>{props.startup.startupName}</h3>
     <a href={props.startup.startupWebSite}>{props.startup.startupWebSite}</a>
