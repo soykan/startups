537974666462e79f4040be72979c3e60852847ff
soykan <soykanerturk@gmail.com>
2023-03-06 20:18:28 +0300

    frontend: search functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+
+const infoOfStartups = [
+  {"startupName": "Getir", 
+  "startupWebSite": "https://getir.uk"},
+
+  {"startupName": "YemekSepeti", 
+  "startupWebSite": "https://yemeksepeti.com"}
+]
 
 function App() {
   return (
     <div>
+      <Search />
     </div>
   );
 }
+
+
+const Search = () => {
+  const [foundStartups, setFoundStartups] = React.useState([]);
+
+  const searchStartups = (event) => {
+    event.preventDefault();
+    setFoundStartups(searchStartup(event.target.elements.startupName.value));
+  } 
+
+  return (
+    <div>
+      <form onSubmit={(event) => searchStartups(event)}>
+        <input type="text" placeholder="startup name" name="startupName" />
+        <input type="submit" value="Search" />
+      </form>
+    <div>
+      {foundStartups.map((startup) => {
+        return (
+          <StartupDetails startup={startup} />
+        )
+      })}
+    </div>
+    </div>
+  )
+}
+
+
+const StartupDetails = (props) => (
+  <div>
+    <h3>{props.startup.startupName}</h3>
+    <a href={props.startup.startupWebSite}>{props.startup.startupWebSite}</a>
+  </div>
+)
+
+
+const searchStartup = (startupName) => {
+  let foundStartups = [];
+  const allStartupInformations = infoOfStartups;
+  for (let i=0; i<allStartupInformations.length; i++) {
+    if (allStartupInformations[i].startupName.toLowerCase().includes(startupName.toLowerCase())) {
+      foundStartups.push(allStartupInformations[i]);
+    }
+  }
+  return foundStartups;
+}
+
 
 export default App;
