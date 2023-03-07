0e9ba6b19aa29a68a5f42ce75a1bc511cc2db65a
soykan <soykanerturk@gmail.com>
2023-03-07 12:57:16 +0300

    secret key changed and stored in file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
 
-with open('secret_key.txt') as f:
+with open(BASE_DIR / 'secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 
