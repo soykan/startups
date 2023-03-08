c11f4ec876dc53a40e8b995d6f057f7c16db73f9
soykan <soykanerturk@gmail.com>
2023-03-09 01:33:57 +0300

    search results from url path implemented

--- frontend/src/pages/SearchResults.js
+++ frontend/src/pages/SearchResults.js
@@ -1,9 +1,58 @@
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
 
+const INFO_OF_STARTUPS = [
+  {
+    "startupId": 0,
+    "startupName": "Getir", 
+    "startupWebSite": "https://getir.uk"
+  },
+
+  {
+    "startupId": 1,
+    "startupName": "YemekSepeti", 
+    "startupWebSite": "https://yemeksepeti.com"
+  }
+]
+
+
 const SearchResults = () => {
   let { searchText } = useParams();
-  return <h1>Search results: {searchText}</h1>;
-};
+  const searchResult = searchStartup(searchText);
   
-  export default SearchResults;
+  return (
+    <div>
+      {searchResult.map((startup) => {
+        return <h3>{startup.startupName}</h3>;
+      })
+      }
+    </div>
+  );
+};
+
+
+const searchStartup = (searchText) => {
+  const allStartupInformations = INFO_OF_STARTUPS;
+  let searchResult = allStartupInformations.filter((startupInformation) => {
+    if (checkStringIncludesAnotherWithoutCaseSensitivity(startupInformation.startupName, searchText)) {
+      return true;
+    }
+    return false;
+  });
+  return searchResult;
+}
+
+
+const checkStringIncludesAnotherWithoutCaseSensitivity = (mainString, searched) => {
+  let mainStringLowerCased = getTurkishLowerCasedString(mainString);
+  let searchStringLowerCased = getTurkishLowerCasedString(searched);
+  return mainStringLowerCased.includes(searchStringLowerCased);
+}
+
+
+const getTurkishLowerCasedString = (text) => {
+  return text.toLocaleLowerCase("tr");
+}
+
+
+export default SearchResults;
