0a7fb09c76bc0ba43e97c129b647ccbf615b26f2
soykan <soykanerturk@gmail.com>
2023-03-08 23:03:17 +0300

    project structures for routing support

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,14 +7,19 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
 import StartupInformation from './pages/StartupInformation';
+import SearchResults from './pages/SearchResults';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<StartupInformation />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/startup-information" element={<StartupInformation />} />
+        <Route path="/search-results" element={<SearchResults />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ frontend/src/pages/SearchResults.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const SearchResults = () => {
+    return <h1>Search results will be here</h1>;
+  };
+  
+  export default SearchResults;
