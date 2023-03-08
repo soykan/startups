0971d7402aa68201bc7fa451f97aa857eb9fef9d
soykan <soykanerturk@gmail.com>
2023-03-08 23:20:45 +0300

    design

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -2,11 +2,43 @@ import {Outlet, Link} from "react-router-dom";
 
 const Layout = () => (
   <div>
-  <Link to="/">Home Page</Link>
-  <Link to="/startup-information">Startup Information</Link>
-  <Link to="/search-results">Search Results</Link>
-  <Outlet />
+    <SiteHeader />
+
+    <Outlet />
+
+    <SiteFooter />
   </div>
+)
+
+const SiteHeader = () => (
+  <div>
+    <span>Site Header:</span>
+
+    <Spacing />
+
+    <Link to="/">Home Page</Link>
+
+    <Spacing />
+
+    <Link to="/startup-information">Startup Information</Link>
+
+    <Spacing />
+
+    <Link to="/search-results">Search Results</Link>
+
+    <Spacing />
+  </div>
+)
+
+
+const SiteFooter = () => (
+  <div>
+    Site Footer
+  </div>
+)
+
+const Spacing = () => (
+  <>&nbsp;&nbsp;&nbsp;</>
 )
 
 export default Layout;
