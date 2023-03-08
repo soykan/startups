6b660283851565c11e3b2e0c4aaf31f4d9aab855
soykan <soykanerturk@gmail.com>
2023-03-09 01:40:06 +0300

    search form edit

--- frontend/src/pages/SearchForm.js
+++ frontend/src/pages/SearchForm.js
@@ -1,10 +1,16 @@
 import * as React from 'react';
 
-const SearchForm = (props) => (
-    <form onSubmit={(event) => props.handleSearchFormSubmit(event)}>
+const SearchForm = () => (
+    <form onSubmit={(event) => handleSearchFormSubmit(event)}>
       <input type="text" placeholder="startup name" name="startupName" />
       <input type="submit" value="Search" />
     </form>
 )
+
+const handleSearchFormSubmit = (event) => {
+  // WILL BE IMPLEMENTED (get search text by using the event)
+}
+
+
 
 export default SearchForm;
