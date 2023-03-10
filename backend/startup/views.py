7c5714402ac33435367454c36e7cd06eb9e55d85
soykan <soykanerturk@gmail.com>
2023-03-10 21:20:27 +0300

    rest related operations

--- backend/startup/views.py
+++ backend/startup/views.py
@@ -3,8 +3,9 @@ from rest_framework import permissions
 from startup.serializers import StartupDetailSerializer, StartupTagSerializer
 from startup.models import StartupDetail, StartupTag
 
+
 class StartupDetailViewSet(viewsets.ModelViewSet):
-    queryset = StartupDetail.objects.filter(name__icontains = 'deneme')
+    queryset = StartupDetail.objects.filter(name__icontains = 'deneme')    
     serializer_class = StartupDetailSerializer
     permission_classes = [permissions.IsAuthenticated]
 
