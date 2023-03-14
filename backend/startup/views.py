43563f4ef0fa29e9ca667b2627a06a6a99d6c3aa
soykan <soykanerturk@gmail.com>
2023-03-15 00:13:20 +0300

    dockerized

--- backend/startup/views.py
+++ backend/startup/views.py
@@ -5,12 +5,12 @@ from startup.models import StartupDetail, StartupTag
 
 
 class StartupDetailViewSet(viewsets.ModelViewSet):
-    queryset = StartupDetail.objects.filter(name__icontains = 'deneme')    
+    queryset = StartupDetail.objects.all()
     serializer_class = StartupDetailSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
 class StartupTagViewSet(viewsets.ModelViewSet):
-    queryset = StartupTag.objects.filter(startup__name = 'startup ismi')
+    queryset = StartupTag.objects.all()
     serializer_class = StartupTagSerializer
     permission_classes = [permissions.IsAuthenticated]
